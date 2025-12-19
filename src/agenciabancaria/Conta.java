300a18a3275d4bff131f185f2e115c3fa172f34b
Marinho <ceciliamarinho1105@gmail.com>
2025-12-19 18:16:22 -0300

    Update Conta.java

--- src/agenciabancaria/Conta.java
+++ src/agenciabancaria/Conta.java
@@ -1,68 +1,51 @@
 package agencia_bancaria;
 
-import javax.swing.JOptionPane;
+public class Conta {
 
-public class Conta {  
-        private int numeroConta;
-        private double saldo;
-    
-    
-        public Conta(int numeroConta, double saldo) {
-            this.numeroConta = numeroConta;
-            this.saldo = saldo;
+    private int numeroConta;
+    private double saldo;
+
+    public Conta(int numeroConta, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldoInicial;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            return true;
         }
-    
-    
-    
-        public int getNumeroConta() {
-            return numeroConta;
+        return false;
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            return true;
         }
-    
-        public double getSaldo() {
-            return saldo;
+        return false;
+    }
+
+    public boolean transferir(double valor, Conta destino) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            destino.saldo += valor;
+            return true;
         }
-    
-        public void setSaldo(double saldo) {
-            this.saldo = saldo;
-        }
-    
-    
-        public void depositar(double valor) {
-            if (valor > 0) {
-                setSaldo(getSaldo() + valor);
-                JOptionPane.showMessageDialog(null, "Seu depósito foi realizado com sucesso!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Não foi possível realizar o depósito.");
-            }
-        }
-    
-    
-    
-        public void sacar(double valor) {
-            if (valor > 0 && valor <= saldo) {
-                setSaldo(getSaldo() - valor);
-                JOptionPane.showMessageDialog(null, "Saque realizado com sucesso!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Saldo insuficiente ou valor inválido.");
-            }
-        }
-    
-    
-        public void transferir(double valor, Conta destino) {
-            if (valor > 0 && valor <= saldo) {
-                this.saldo -= valor;
-                destino.saldo += valor;
-                JOptionPane.showMessageDialog(null, "Transferência realizada com sucesso!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Não foi possível realizar a transferência.");
-            }
-        }
-    
-    
-        @Override
-        public String toString() {
-            return "Número da Conta: " + numeroConta +
-                   "\nSaldo: R$ " + saldo;
-        }
-    
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta: " + numeroConta +
+               "\nSaldo: R$ " + String.format("%.2f", saldo);
+    }
 }
